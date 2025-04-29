21c86e9dacdceb02c0f2917324ac717729517351
Kjw072892 <kassie.j.whitney92@gmail.com>
2025-04-29 16:01:43 -0700

    completed assignment 2

--- Assignments/Assignments_2/QueueADT.java
+++ Assignments/Assignments_2/QueueADT.java
@@ -12,35 +12,35 @@ public interface QueueADT<T>
      * Adds one element to the rear of this queue. 
      * @param element  the element to be added to the rear of the queue  
      */
-    public void enqueue(T element);
+    void enqueue(T element);
 
     /**  
      * Removes and returns the element at the front of this queue.
      * @return the element at the front of the queue
      */
-    public T dequeue();
+    T dequeue();
 
     /**  
      * Returns without removing the element at the front of this queue.
      * @return the first element in the queue
      */
-    public T first();
+    T first();
    
     /**  
      * Returns true if this queue contains no elements.
      * @return true if this queue is empty
      */
-    public boolean isEmpty();
+    boolean isEmpty();
 
     /**  
      * Returns the number of elements in this queue. 
      * @return the integer representation of the size of the queue
      */
-    public int size();
+    int size();
 
     /**  
      * Returns a string representation of this queue. 
      * @return the string representation of the queue
      */
-    public String toString();
+    String toString();
 }
